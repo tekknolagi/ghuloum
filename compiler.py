d5b610d09bc6b1c5a4cbb23b05dc529e9557589a
Max Bernstein <max@bernsteinbear.com>
2025-08-10 13:22:09 -0400

    Don't pass -O0 to linker
    It doesn't mean anything.

--- compiler.py
+++ compiler.py
@@ -221,7 +221,7 @@ def link(program, outfile=None, verbose=True):
         run(["ccache", "clang", "-O0", "-ggdb", "-c", "runtime.c"], verbose=verbose)
         compiled_object = f"{f.name}.o"
         run(["ccache", "clang", "-O0", "-masm=intel", f.name, "-c", "-o", compiled_object], verbose=verbose)
-        run(["ccache", "clang", "-O0", "-masm=intel", "-no-pie", compiled_object, "runtime.o", "-o", outfile], verbose=verbose)
+        run(["ccache", "clang", "-masm=intel", "-no-pie", compiled_object, "runtime.o", "-o", outfile], verbose=verbose)
     return outfile
 
 class EndToEndTests(unittest.TestCase):
