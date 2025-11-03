6831ebb251b14c044c1199e758f4dd9aa87e12fc
Max Bernstein <ruby@bernsteinbear.com>
2025-11-02 23:53:26 -0500

    Continue bytecode compiler

--- bccompiler.py
+++ bccompiler.py
@@ -15,7 +15,11 @@ CONS_TAG = 0b001
 CLOSURE_TAG = 0b110
 HEAP_ALIGNMENT = 2 * WORD_SIZE
 
+def is_fixnum(v):
+    return (v & FIXNUM_MASK) == FIXNUM_TAG
+
 def box_fixnum(n):
+    assert isinstance(n, int)
     # TODO(max): Check range
     return (n << FIXNUM_SHIFT) | FIXNUM_TAG
 
@@ -23,7 +27,11 @@ def unbox_fixnum(v):
     assert (v & FIXNUM_MASK) == FIXNUM_TAG
     return v >> FIXNUM_SHIFT
 
+def is_char(v):
+    return (v & 0xFF) == CHAR_TAG
+
 def box_char(c):
+    assert isinstance(c, Char)
     return (c.byte << CHAR_SHIFT) | CHAR_TAG
 
 def unbox_char(v):
@@ -31,16 +39,16 @@ def unbox_char(v):
     byte = (v >> CHAR_SHIFT) & 0xFF
     return Char(chr(byte))
 
-def immediate_rep(val):
-    match val:
-        case bool(_):
-            return (val << BOOL_SHIFT) | BOOL_TAG
-        case int(_):
-            return box_fixnum(val)
-        case Char():
-            return box_char(val)
-        case _:
-            raise NotImplementedError(val)
+def is_bool(v):
+    return (v & BOOL_MASK) == BOOL_TAG
+
+def box_bool(b):
+    assert isinstance(b, bool)
+    return (b << BOOL_SHIFT) | BOOL_TAG
+
+def unbox_bool(v):
+    assert (v & BOOL_MASK) == BOOL_TAG
+    return bool((v >> BOOL_SHIFT) & 0x1)
 
 class Char:
     def __init__(self, c):
@@ -66,14 +74,25 @@ class I:
     PRIM_ADD1, \
     PRIM_INTEGER_TO_CHAR, \
     PRIM_CHAR_TO_INTEGER, \
+    PRIM_IS_NULL, \
+    PRIM_IS_ZERO, \
+    PRIM_NOT, \
+    PRIM_IS_INTEGER, \
+    PRIM_IS_BOOLEAN, \
     *_ = range(1000)
 
 def compile_expr(expr, code, si, env):
     emit = code.append
     match expr:
-        case int(_) | Char():
+        case bool(_):
             emit(I.LOAD64)
-            emit(immediate_rep(expr))
+            emit(box_bool(expr))
+        case int(_):
+            emit(I.LOAD64)
+            emit(box_fixnum(expr))
+        case Char():
+            emit(I.LOAD64)
+            emit(box_char(expr))
         case ["add1", e]:
             compile_expr(e, code, si, env)
             emit(I.PRIM_ADD1)
@@ -83,6 +102,18 @@ def compile_expr(expr, code, si, env):
         case ["char->integer", e]:
             compile_expr(e, code, si, env)
             emit(I.PRIM_CHAR_TO_INTEGER)
+        case ["zero?", e]:
+            compile_expr(e, code, si, env)
+            emit(I.PRIM_IS_ZERO)
+        case ["not", e]:
+            compile_expr(e, code, si, env)
+            emit(I.PRIM_NOT)
+        case ["integer?", e]:
+            compile_expr(e, code, si, env)
+            emit(I.PRIM_IS_INTEGER)
+        case ["boolean?", e]:
+            compile_expr(e, code, si, env)
+            emit(I.PRIM_IS_BOOLEAN)
         case _:
             raise NotImplementedError(expr)
 
@@ -107,10 +138,16 @@ def interpret(code):
                 push(box_fixnum(unbox_fixnum(v) + 1))
             case I.PRIM_INTEGER_TO_CHAR:
                 v = stack.pop()
-                push(immediate_rep(Char(chr(unbox_fixnum(v)))))
+                push(box_char(Char(chr(unbox_fixnum(v)))))
             case I.PRIM_CHAR_TO_INTEGER:
                 v = stack.pop()
                 push(box_fixnum(unbox_char(v).byte))
+            case I.PRIM_IS_ZERO:
+                v = stack.pop()
+                push(box_bool(v == box_fixnum(0)))
+            case I.PRIM_NOT:
+                v = stack.pop()
+                push(box_bool(not unbox_bool(v)))
             case _:
                 raise NotImplementedError(instr)
     return stack.pop()
@@ -122,33 +159,56 @@ class EndToEndTests(unittest.TestCase):
         return interpret(bytecode)
 
     # TODO(max): Add assertEqual that understands tagged values
+    
+    def assertTaggedEqual(self, a, b):
+        if is_fixnum(a) and is_fixnum(b):
+            return self.assertEqual(unbox_fixnum(a), unbox_fixnum(b))
+        if is_char(a) and is_char(b):
+            return self.assertEqual(unbox_char(a), unbox_char(b))
+        if is_bool(a) and is_bool(b):
+            return self.assertEqual(unbox_bool(a), unbox_bool(b))
+        self.fail(f"Values not equal: {a} vs {b}")
 
     def test_positive_fixnum(self):
-        self.assertEqual(self._run(42), immediate_rep(42))
+        self.assertTaggedEqual(self._run(42), box_fixnum(42))
 
     def test_negative_fixnum(self):
-        self.assertEqual(self._run(-17), immediate_rep(-17))
+        self.assertTaggedEqual(self._run(-17), box_fixnum(-17))
+
+    def test_boolean_true(self):
+        self.assertTaggedEqual(self._run(True), box_bool(True))
+
+    def test_boolean_false(self):
+        self.assertTaggedEqual(self._run(False), box_bool(False))
 
     def test_character(self):
-        self.assertEqual(self._run(Char('A')), immediate_rep(Char('A')))
+        self.assertTaggedEqual(self._run(Char('A')), box_char(Char('A')))
 
     def test_add1(self):
-        self.assertEqual(self._run(["add1", 41]), immediate_rep(42))
+        self.assertTaggedEqual(self._run(["add1", 41]), box_fixnum(42))
 
     def test_nested_add1(self):
-        self.assertEqual(self._run(["add1", ["add1", 3]]), immediate_rep(5))
+        self.assertTaggedEqual(self._run(["add1", ["add1", 3]]), box_fixnum(5))
 
     def test_integer_to_char(self):
-        self.assertEqual(self._run(["integer->char", 65]), immediate_rep(Char('A')))
+        self.assertTaggedEqual(self._run(["integer->char", 65]), box_char(Char('A')))
 
     def test_nested_integer_to_char(self):
-        self.assertEqual(self._run(["integer->char", ["add1", 65]]), immediate_rep(Char('B')))
+        self.assertTaggedEqual(self._run(["integer->char", ["add1", 65]]), box_char(Char('B')))
 
     def test_char_to_integer(self):
-        self.assertEqual(self._run(["char->integer", Char('A')]), immediate_rep(65))
+        self.assertTaggedEqual(self._run(["char->integer", Char('A')]), box_fixnum(65))
 
     def test_nested_char_to_integer(self):
-        self.assertEqual(self._run(["char->integer", ["integer->char", 66]]), immediate_rep(66))
+        self.assertTaggedEqual(self._run(["char->integer", ["integer->char", 66]]), box_fixnum(66))
+
+    def test_is_zero(self):
+        self.assertTaggedEqual(self._run(["zero?", ["add1", -1]]), box_bool(True))
+        self.assertTaggedEqual(self._run(["zero?", ["add1", 0]]), box_bool(False))
+
+    def test_not(self):
+        self.assertTaggedEqual(self._run(["not", ["zero?", 0]]), box_bool(False))
+        self.assertTaggedEqual(self._run(["not", ["zero?", 1]]), box_bool(True))
 
 if __name__ == "__main__":
     unittest.main()
