aba5b3f82ad0b99d5d6cb3bf4ebae79b04d42848
Max Bernstein <max@bernsteinbear.com>
2025-11-15 12:22:08 -0500

    Compile to C

---
+++ ccompiler.py
@@ -0,0 +1,165 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "unittest-parallel",
+# ]
+# ///
+import tempfile
+import unittest
+from run import run
+
+WORD_SIZE = 8
+FIXNUM_SHIFT = 2
+FIXNUM_MASK = 0b11
+FIXNUM_TAG = 0b00
+CHAR_TAG = 0b00001111
+CHAR_SHIFT = 8
+BOOL_TAG = 0b0011111
+BOOL_SHIFT = 7
+BOOL_MASK = 0b1111111
+BOOL_BIT = 1 << BOOL_SHIFT
+EMPTY_LIST = 0b00101111
+CONS_TAG = 0b001
+CLOSURE_TAG = 0b110
+HEAP_ALIGNMENT = 2 * WORD_SIZE
+
+def immediate_rep(val):
+    match val:
+        case bool(_):
+            return (val << BOOL_SHIFT) | BOOL_TAG
+        case int(_):
+            return val << FIXNUM_SHIFT
+        case Char():
+            return (val.byte << CHAR_SHIFT) | CHAR_TAG
+        case _:
+            raise NotImplementedError(val)
+
+class Char:
+    def __init__(self, c):
+        b = c.encode("utf-8")
+        assert len(b) == 1
+        self.byte = b[0]
+
+    def __eq__(self, other):
+        return isinstance(other, Char) and self.byte == other.byte
+
+    def __repr__(self):
+        return f"Char({self.byte})"
+
+BUILTINS = frozenset({
+    "add1", "integer->char",
+    "char->integer", "null?", "zero?",
+    "not", "integer?", "boolean?", "+",
+    "cons", "car", "cdr",
+})
+
+def compile_expr(expr, code, env):
+    emit = code.append
+    def comment(msg):
+        emit(f"// {msg}")
+    def unique_label():
+        global NEXT_LABEL
+        NEXT_LABEL += 1
+        return f"L{NEXT_LABEL}"
+    match expr:
+        case int(_) | Char():
+            return immediate_rep(expr)
+            # emit(f"mov {ACC}, {immediate_rep(expr)}")
+        case _:
+            raise NotImplementedError(expr)
+
+def lift_lambdas_rec(expr, labels, bound, free):
+    match expr:
+        case int(_) | Char():
+            return expr
+        case str(_) if expr in bound or expr in BUILTINS:
+            return expr
+        case str(_):
+            free.add(expr)
+            return expr
+        case ["lambda", params, body]:
+            body_free = set()
+            assert all(isinstance(v, str) for v in params)
+            body = lift_lambdas_rec(body, labels, set(params), body_free)
+            assert all(isinstance(v, str) for v in body_free)
+            free.update(body_free - bound)
+            body_free = sorted(body_free)
+            label = f"f{len(labels)}"
+            labels[label] = ["code", params, body_free, body]
+            return ["closure", label, *body_free]
+        case ["let", bindings, body]:
+            new_bindings = []
+            names = {name for name, _ in bindings}
+            for name, val_expr in bindings:
+                new_bindings.append([name, lift_lambdas_rec(val_expr, labels, bound, free)])
+            new_body = lift_lambdas_rec(body, labels, bound | names, free)
+            return ["let", new_bindings, new_body]
+        case ["if", test, conseq, alt]:
+            return ["if",
+                    lift_lambdas_rec(test, labels, bound, free),
+                    lift_lambdas_rec(conseq, labels, bound, free),
+                    lift_lambdas_rec(alt, labels, bound, free)]
+        case [func, *args]:
+            result = [] if isinstance(func, str) and func in BUILTINS else ["funcall"]
+            for e in expr:
+                result.append(lift_lambdas_rec(e, labels, bound, free))
+            return result
+        case _:
+            raise NotImplementedError(expr)
+
+def lift_lambdas(expr):
+    labels = {}
+    expr = lift_lambdas_rec(expr, labels, set(), set())
+    labels = [[name, code] for name, code in labels.items()]
+    return ["labels", labels, expr]
+
+def compile_program(expr):
+    code = [
+        open("cruntime.c", "r").read()
+    ]
+    match expr:
+        case ["labels", labels, body]:
+            for (lvar, lexpr) in labels:
+                code.append(f"void {lvar}() {{")
+                compile_lexpr(lexpr, code)
+                code.append("}")
+            code.append("Object scheme_entry(Object *closure) {")
+            result = compile_expr(body, code, env={})
+            code.append(f"return {result};")
+            code.append("}")
+        case _:
+            expr = lift_lambdas(expr)
+            assert isinstance(expr, list) and expr[0] == "labels"
+            return compile_program(expr)
+    return "\n".join(code)
+
+HAVE_CCACHE = True
+
+def link(program, outfile=None, verbose=True):
+    if not outfile:
+        outfile = "a.out"
+    ccache = ["ccache"] if HAVE_CCACHE else []
+    with tempfile.NamedTemporaryFile(suffix=".c") as f:
+        f.write(program.encode("utf-8"))
+        f.flush()
+        run([*ccache, "clang", "-O0", f.name, "-o", outfile], verbose=verbose)
+    return outfile
+
+class EndToEndTests(unittest.TestCase):
+    def _run(self, expr):
+        return self._run_program(["labels", [], expr])
+
+    def _run_program(self, program):
+        asm = compile_program(program)
+        with tempfile.NamedTemporaryFile(suffix=".out", delete_on_close=False) as f:
+            link(asm, f.name, verbose=False)
+            f.close()
+            result = run([f.name], verbose=False, capture_output=True)
+        self.assertIsNot(result.stdout, None)
+        return result.stdout.removesuffix("\n")
+
+    def test_int(self):
+        self.assertEqual(self._run(123), "123")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ cruntime.c
@@ -0,0 +1,103 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef uint64_t Object;
+
+extern Object scheme_entry(Object *closure);
+
+#define fixnum_mask 3
+#define fixnum_tag 0
+#define fixnum_shift 2
+#define char_tag 0xf
+#define char_shift 8
+#define obj_false 0x1f
+#define obj_true 0x9f
+#define empty_list 0x2f
+#define heap_mask 7
+#define cons_tag 0x1
+#define vector_tag 0x2
+#define string_tag 0x3
+#define symbol_tag 0x5
+#define closure_tag 0x6
+
+bool is_fixnum(Object obj) {
+  return (obj & fixnum_mask) == fixnum_tag;
+}
+
+int64_t unbox_fixnum(Object obj) {
+  assert(is_fixnum(obj));
+  return ((int64_t)obj) >> fixnum_shift;
+}
+
+bool is_char(Object obj) {
+  return (obj & 0xff) == char_tag;
+}
+
+char unbox_char(Object obj) {
+  assert(is_char(obj));
+  return obj >> char_shift;
+}
+
+bool is_cons(Object obj) {
+  return (obj & heap_mask) == cons_tag;
+}
+
+Object* unbox_heap(Object obj) {
+  return (Object*)(obj & ~heap_mask);
+}
+
+Object car(Object obj) {
+  assert(is_cons(obj));
+  return unbox_heap(obj)[0];
+}
+
+Object cdr(Object obj) {
+  assert(is_cons(obj));
+  return unbox_heap(obj)[1];
+}
+
+bool is_closure(Object obj) {
+  return (obj & heap_mask) == closure_tag;
+}
+
+bool is_empty_list(Object obj) {
+  return obj == empty_list;
+}
+
+void print_obj(Object obj) {
+  FILE *fp = stdout;
+  if (is_fixnum(obj)) {
+    fprintf(fp, "%ld", unbox_fixnum(obj));
+  } else if (is_char(obj)) {
+    fprintf(fp, "'%c'", unbox_char(obj));
+  } else if (obj == obj_true) {
+    fprintf(fp, "#t");
+  } else if (obj == obj_false) {
+    fprintf(fp, "#f");
+  } else if (is_empty_list(obj)) {
+    fprintf(fp, "()");
+  } else if (is_cons(obj)) {
+    fprintf(fp, "(");
+    print_obj(car(obj));
+    fprintf(fp, " . ");
+    print_obj(cdr(obj));
+    fprintf(fp, ")");
+  } else if (is_closure(obj)) {
+    fprintf(fp, "<closure>");
+  } else {
+    fprintf(fp, "<unknown %p>", (void*)obj);
+  }
+}
+
+void println_obj(Object obj) {
+  FILE *fp = stdout;
+  print_obj(obj);
+  fprintf(fp, "\n");
+}
+
+int main() {
+  Object obj = scheme_entry(NULL);
+  println_obj(obj);
+}
